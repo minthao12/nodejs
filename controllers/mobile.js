7de7418011e8f3c2caed37b3184002e93c879a39
minthao12 <thaolmph35241@fpt.edu.vn>
2024-04-09 09:59:54 +0700

    first commit

--- controllers/mobile.js
+++ controllers/mobile.js
@@ -36,31 +36,35 @@ class MobileController {
         }
     }
     // Post / Mobile 
-    async createMobile(req, res){ 
+    async createMobile(req, res) {
         console.log(req.body);
         try {
             const mobile = await Mobile.create(req.body);
             res.status(200).json({
                 message: "create Mobile",
                 data: mobile,
-            });  
+            });
         } catch (error) {
             res.status(400).json({
                 message: error.message,
-            });       
+            });
+        }
     }
-}
-// Update/Mobile/:id 
-async updatePhones(req, res,){
-    try {
-        const mobile = await Mobile.findByIdAndUpdate(req.params.id, req.body,{
-            new: true,
-        });
-        res.status(200).send({
-            message: "Updatedz"
-    } catch (error) {
-        
+    // Update/Mobile/:id 
+    async updatePhones(req, res,) {
+        try {
+            const mobile = await Mobile.findByIdAndUpdate(req.params.id, req.body, {
+                new: true,
+            });
+            res.status(200).send({
+                message: "Updatedz"
+            });
+
+        }
+        catch (error) {
+
+        }
     }
+
 }
- }
 export default MobileController;
